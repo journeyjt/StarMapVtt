6682c6aad92f0d54c09065179ac5ce314c4a23dc
Jeff Journey <journeyjt@gmail.com>
2024-10-29 12:36:56 -0400

    Use community types Add init console log

---
+++ dist/scripts/module.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("@league-of-foundry-developers/foundry-vtt-types");
+Hooks.on('ready', function () {
+    console.log("Star Map is ready!");
+});
---
+++ src/scripts/module.ts
@@ -0,0 +1,6 @@
+import '@league-of-foundry-developers/foundry-vtt-types'
+
+Hooks.on('ready', () => {
+    console.log(`Star Map is ready!`);
+})
+
