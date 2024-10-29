a9db29e87260fb97da1908195865530a71c8f3bb
Jeff Journey <journeyjt@gmail.com>
2024-10-29 14:16:20 -0400

    Try a functional approach

--- src/scripts/star-map.ts
+++ src/scripts/star-map.ts
@@ -1,24 +1,20 @@
-import '@league-of-foundry-developers/foundry-vtt-types'
+import "@league-of-foundry-developers/foundry-vtt-types"
 
-export class StarMap {
-  constructor(){
-    console.log("StarMap initialized");
+export function initStarMap() {
 
-    Hooks.once("init", function() {
-      CONFIG.debug.hooks = true;
+  console.log("StarMap initialized");
+
+  Hooks.once("init", function() {
+    CONFIG.debug.hooks = true;
+  });
+  
+  Hooks.on("init", function() {
+      console.log("This code runs once the Foundry VTT software begins its initialization workflow.");
     });
     
-    Hooks.on("init", function() {
-        console.log("This code runs once the Foundry VTT software begins its initialization workflow.");
-      });
-      
-    Hooks.on("ready", function() {
-        console.log("This code runs once core initialization is ready and game data is available.");
-      });
-  }
-}
+  Hooks.on("ready", function() {
+      console.log("This code runs once core initialization is ready and game data is available.");
+    });
+};
 
-
-
-
-
+initStarMap();  // Call the function to initialize the StarMap
