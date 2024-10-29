7ef1db8ff31692a1b3daae1d218b6a9a05fc0e0a
Jeff Journey <journeyjt@gmail.com>
2024-10-29 13:59:16 -0400

    Rename module js file

--- dist/scripts/module.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("@league-of-foundry-developers/foundry-vtt-types");
-Hooks.on('ready', function () {
-    console.log("Star Map is ready!");
-});
--- src/scripts/module.ts
+++
@@ -1,6 +0,0 @@
-import '@league-of-foundry-developers/foundry-vtt-types'
-
-Hooks.on('ready', () => {
-    console.log(`Star Map is ready!`);
-})
-
